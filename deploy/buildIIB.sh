96995fa8e68f24a61635e3b3b3df23e7354f2add
CALLUM JACKSON <callumj@uk.ibm.com>
2017-09-08 14:14:55 +0100

    .

--- deploy/buildIIB.sh
+++ deploy/buildIIB.sh
@@ -1,4 +1,5 @@
-. /opt/ibm/iib-10.0.0.9/server/bin/mqsiprofile
+cd /opt/ibm/iib-10.0.0.9/server/bin
+. ./mqsiprofile
 mqsipackagebar -a iibApp.bar -w integration -k RESTAPI
 scp iibApp.bar callumj@9.19.34.117:/storage/CASE/refarch-privatecloud
 expect "Password: "
