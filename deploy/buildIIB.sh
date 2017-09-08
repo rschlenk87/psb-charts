cf10f4bd6075bb07352276e71bca24f8affd4d6a
CALLUM JACKSON <callumj@uk.ibm.com>
2017-09-08 17:36:11 +0100

    .

--- deploy/buildIIB.sh
+++ deploy/buildIIB.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . '/opt/ibm/iib-10.0.0.9/server/bin/mqsiprofile'
-mqsipackagebar -a iibApp.bar -w /tmp/git/refarch-integration-esb/integration -k RESTAPI
+mqsipackagebar -a iibApp.bar -w integration -k RESTAPI
