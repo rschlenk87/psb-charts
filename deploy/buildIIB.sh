a316a110c4405f143dc0f20c899644510e8e5ae9
CALLUM JACKSON <callumj@uk.ibm.com>
2017-09-08 14:09:22 +0100

    .

--- deploy/buildIIB.sh
+++ deploy/buildIIB.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 . /opt/ibm/iib-10.0.0.9/server/bin/mqsiprofile
 mqsipackagebar -a iibApp.bar -w integration -k RESTAPI
 scp iibApp.bar callumj@9.19.34.117:/storage/CASE/refarch-privatecloud
