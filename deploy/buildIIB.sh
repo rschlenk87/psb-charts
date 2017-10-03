ebf25efc665f37393ce9f0da3fe4c1c4720b4f0e
Robert Schlenker <vwlp37b@vwag.com>
2017-10-03 18:35:43 +0200

    next try..

--- deploy/buildIIB.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-. '/opt/ibm/iib-10.0.0.9/server/bin/mqsiprofile'
-mqsipackagebar -a iibApp.bar -w integration -k RESTAPI
--- docker/mq/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Change log
-
-## 1.0.0 (2017-08-29)
-* Initial version customized from IBM MQ Docker version
